0c03019c691d37a03d457fa6ae5cbf4791022c26
Steven Schwab <schwabs@umich.edu>
2025-04-11 11:56:44 -0400

    add insert methods for beginning, middle, and end

--- linkedLists.js
+++ linkedLists.js
@@ -6,6 +6,7 @@ class Node {
     }
 }
 
+// Linked List Class
 class LinkedList {
     constructor() {
         this.head = null
@@ -24,6 +25,59 @@ class LinkedList {
                 current = current.next
             }
             current.next = newNode // insert the new node to the last node's next variable
+        }
+        this.size++
+    }
+
+    // Insert node at beginning, middle, or end
+    insertAtBeginning(data) {
+        const newNode = new Node(data);
+        newNode.next = this.head;
+        this.head = newNode;
+        this.size++;
+    }
+
+    // Insert at the middle (at a specific position)
+    insertAtMiddle(data, position) {
+        if (position < 0 || position > this.size) {
+            console.log(`Invalid position. Position should be between 0 and ${this.size}`)
+            return false
+        }
+
+        if (position === 0) {
+            this.insertAtBeginning(data)
+            return true
+        }
+
+        const newNode = new Node(data)
+        let current = this.head;
+        let previous = null;
+        let currentPosition = 0;
+
+        while (currentPosition < position) {
+            previous = current
+            current = current.next
+            currentPosition++
+        }
+
+        previous.next = newNode;
+        newNode.next = current
+        this.size++
+        return true
+    }
+
+    // Insert at the end
+    insertAtEnd(data) {
+        const newNode = new Node(data)
+
+        if (!this.head) {
+            this.head = newNode
+        } else {
+            let current = this.head;
+            while (current.next) {
+                current = current.next
+            }
+            current.next = newNode
         }
         this.size++
     }
